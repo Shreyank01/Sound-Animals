18f5d513100ff6284ebca4144e26d8cd6dc63541
Shreyank01 <87267295+Shreyank01@users.noreply.github.com>
2022-01-26 20:19:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-function startClassification()  {
- navigator.mediaDevices.getUserMedia({ audio: true});
- classifier = ml5.soundClassifier('', modelReady); 
-}
+function startClassification() 
+{ navigator.mediaDevices.getUserMedia({ audio: true});
+ classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/hCrRxnojd/model.json', modelReady); } 
+ 
+ function modelReady()
+{ classifier.classify( gotResults); }
 
- function modelReady() { 
-    classifier.classify( gotResults); 
-}
+
 
 function gotResults(error,results) {
     if (error){
@@ -14,31 +14,46 @@ function gotResults(error,results) {
     else {
         console.log(results);
 
-        
         random_number_r = Math.floor(Math.random()* 255) + 1;
         random_number_g = Math.floor(Math.random()* 255) + 1;
         random_number_b = Math.floor(Math.random()* 255) + 1;
 
-        document.getElementById("text_confidence").innerHTML = "Sound Matching -" + 
+        document.getElementById("text_name").innerHTML = "I Can Hear" + 
+        results[0].label;
+        document.getElementById("text_accurecy").innerHTML = "Accuracy" + 
         (results[0].confidence * 100 ).toFixed(2) + "%";
-        document.getElementById("text_name").innerHTML = "It's " + 
-        results[0].label ;
         document.getElementById("text_name").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
-        document.getElementById("text_confidence").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
+        document.getElementById("text_accurecy").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
 
-        img1 = document.getElementById("main_img");
+        img1 = document.getElementById("alien_1");
+        img2 = document.getElementById("alien_2");
+        img3 = document.getElementById("alien_3");
+        img4 = document.getElementById("alien_4");
         
-        if(results[0].label == 'Roaring') {
-            img1.src = "lion-roar-clipart-11.jpg";
+        if(results[0].label == 'Clap') {
+            img1.src = "aliens-01.gif";
+            img2.src = "aliens-02.png";
+            img3.src = "aliens-03.png";
+            img4.src = "aliens-04.png";
         }
-        else if(results[0].label == 'Barking') {
-            img1.src = "barking.png";  
+        else if(results[0].label == 'Snap') {
+            img1.src = "aliens-01.png";
+            img2.src = "aliens-02.gif";
+            img3.src = "aliens-03.png";
+            img4.src = "aliens-04.png";
         }
-           else if(results[0].label == 'Mooing') {
-            img1.src = "mooing.png";
+           else if(results[0].label == 'Bell') {
+            img1.src = "aliens-01.png";
+            img2.src = "aliens-02.png";
+            img3.src = "aliens-03.gif";
+            img4.src = "aliens-04.png";
         }
            else{
-            img1.src = "mwowing.png";
+            img1.src = "aliens-01.png";
+            img2.src = "aliens-02.png";
+            img3.src = "aliens-03.png";
+            img4.src = "aliens-04.gif";
         }
+
     }
 }
